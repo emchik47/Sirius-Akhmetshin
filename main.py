3dd234930b93f50cece1a5b3345190678ba5f1a1
yandex.student <emil2007emil2@mail.ru>
2023-11-16 17:59:41 +0300

    Repos.problem2

--- main.py
+++ main.py
@@ -1,7 +1,45 @@
-def main():
-    print('My first git program')
-    print('My second git program')
-    print(5)
+import sys
+import random
 
-if __name__ == "__main__":
-    main()
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 1000, 1000)
+        self.setWindowTitle('Рисование')
+        self.btn = QPushButton('Рисовать', self)
+        self.btn.setGeometry(400, 400, 200, 100)
+        self.do_paint = False
+        self.btn.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+        self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def draw_flag(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        x = random.randint(0, 1000)
+        y = random.randint(0, 1000)
+        z = random.randint(0, 1000)
+        qp.drawEllipse(x, y, z, z)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
